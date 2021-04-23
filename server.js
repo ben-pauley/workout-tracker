5bad1d187fcd7d6b72bd9f81cb0c9e1204dc3634
Ben <pauleybj@gmail.com>
2021-04-23 15:51:12 +0100

    created server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./controllers");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});

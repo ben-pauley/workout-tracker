c9f033fa56c232e7048b59271314c7388060ef4d
Ben <pauleybj@gmail.com>
2021-04-24 11:49:29 +0100

    completed exercise model

---
+++ models/exercise.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  type: {
+    type: String,
+    default: "",
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Please set Exercise name.",
+  },
+  duration: {
+    type: Number,
+    default: 0.0,
+  },
+  weight: {
+    type: Number,
+    default: 0.0,
+  },
+  reps: {
+    type: Number,
+    default: 0,
+  },
+  sets: {
+    type: Number,
+    default: 0,
+  },
+  distance: {
+    type: Number,
+    default: 0.0,
+  },
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = {
+  Exercise,
+  ExerciseSchema,
+};

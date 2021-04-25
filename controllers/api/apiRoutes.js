15f0d674b0ec96c4bb80862c1b50c4ccaf56f802
Ben <pauleybj@gmail.com>
2021-04-25 14:28:13 +0100

    completed api routes

---
+++ controllers/api/apiRoutes.js
@@ -0,0 +1,50 @@
+const Workout = require("../../models/workout");
+const express = require("express");
+const router = express.Router();
+
+router.post("/workouts", (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/workouts/:id", (req, res) => {
+  Workout.updateOne(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/workouts", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;

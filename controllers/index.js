aa54827077e1fd6a1da0042380388734cdcb25b6
Ben <pauleybj@gmail.com>
2021-04-25 14:28:34 +0100

    completed remaining routes

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api/apiRoutes");
+const homeRoutes = require("./homeRoutes");
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+module.exports = router;

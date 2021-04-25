f5b6d5c6abbbef78792d50ba85b4eb2f14337eee
Ben <pauleybj@gmail.com>
2021-04-25 14:27:53 +0100

    completed server file

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const routes = require("./controllers");
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
 app.use(routes);
 
 app.listen(PORT, () => {
